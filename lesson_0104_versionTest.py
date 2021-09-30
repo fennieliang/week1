794a2ad21666f5afbc021f62c95dea347204f932
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:05:48 +0800

    Create lesson_0104_versionTest.py

---
+++ lesson_0104_versionTest.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 19 15:36:37 2021
+
+@author: fennieliang
+"""
+#this program is used to check libraries needed for future classes.
+# to use cv2 module, open anaconda terminal then type pip install opencv-python
+# to use PyTorch, open anaconda terminal then type conda install pytorch torchvision -c pytorch 
+
+#import sys
+#print (f"Python version: {sys.version}")
+#the only one use different command for checking version
+
+#import numpy as np
+#print (f"numpy version: {np.__version__}")#the long underscore is typed using two consecutive underscore. 
+
+#import matplotlib
+#print (f"matplotlib version: {matplotlib.__version__}")
+
+#import seaborn as sns
+#print (f"seaborn version: {sns.__version__}")
+
+#import pandas as pd
+#print (f"pandas version: {pd.__version__}")
+
+#import sklearn
+#print (f"sklearn version: {sklearn.__version__}")
+
+#import tensorflow as tf
+#print (f"tensorflow version: {tf.__version__}")
+
+#import keras 
+#print (f"keras version: {keras.__version__}")
+
+#import flask 
+#print (f"flask  version: {flask.__version__}")
+
+
+#print (tf.config.list_physical_devices('CPU'))
+#print (tf.config.list_physical_devices('GPU'))
+

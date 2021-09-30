a4c2bb12d807147430d9f5f3cb98f40401a5b49f
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:05:51 +0800

    Create lesson_0103_spyder.py

---
+++ lesson_0103_spyder.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  9 11:11:34 2021
+
+@author: fennieliang
+"""
+# https://docs.spyder-ide.org/current/index.html
+# open a new folder in your machine
+# create a new python test.py file and save to the newly created folder
+# from Git desktop do commit and push origin
+# check from your git website 

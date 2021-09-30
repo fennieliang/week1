d206a0f30cf01855b3308ad55683fd81a13f9723
fennieliang <fennieliang@googlemail.com>
2021-09-30 17:05:53 +0800

    Create lesson_0102_git.py

---
+++ lesson_0102_git.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 19 15:36:37 2021
+
+@author: fennieliang
+"""
+
+#Git connection for version control
+    #Go to https://github.com/ to register an account
+    #open a repository named Gittest
+    #download git desktop from https://desktop.github.com/
+    #navigate to the repository and see the right-hand side green button
+    #click on the button, then change the local disk to where you want to
+    #open spyder and create a new python program
+    #save the file to the place where set on the git desktop
+    #on the right bottom corner of the desktop, type "test version"
+    #click on the commit to main 
+    #then click on push origin
+    #every change will be shown on the git desktop
+    #up to now, the change will be made simultaneously in the git repository
+    #go to the GitHub website and check it
+    
+    
